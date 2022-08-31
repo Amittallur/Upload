aa731d797d7b1d6505a4755a8c201f97bf008337
Amit Tallur <61627962+Amittallur@users.noreply.github.com>
2022-08-31 19:21:58 +0530

    Updates

---
+++ precache-manifest.d60b66891759bdf1378e7eeb8db2bf8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a3af559a890d2d376c0234b21a7d6c",
+    "url": "/Amittallur/Upload.git/index.html"
+  },
+  {
+    "revision": "98e2aedb69a03771ba2c",
+    "url": "/Amittallur/Upload.git/static/css/main.838a69de.chunk.css"
+  },
+  {
+    "revision": "0bf9b4f328653cc85181",
+    "url": "/Amittallur/Upload.git/static/js/2.e20dd552.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Amittallur/Upload.git/static/js/2.e20dd552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e2aedb69a03771ba2c",
+    "url": "/Amittallur/Upload.git/static/js/main.380aefc3.chunk.js"
+  },
+  {
+    "revision": "13df9e092de78626df3d",
+    "url": "/Amittallur/Upload.git/static/js/runtime-main.99449159.js"
+  }
+]);
